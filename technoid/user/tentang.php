dc7704cadcda601ff841e9791c6ee9072610f1ee
Faisal-88 <faisaltimbangan32@gmail.com>
2022-07-04 12:46:18 +0700

    Project-Akhir-UAS

---
+++ technoid/user/tentang.php
@@ -0,0 +1,31 @@
+<title>Tentang Kami</title>
+<?php
+require '../config/config.php';
+
+
+require 'templates/header.php';
+?>
+
+    
+<div class="row bg-white border p-2 mt-5">
+    <div class="col-md ml-3">
+        <h1>Tentang Kami</h1>
+        <p class="lead">Kami menjual berbagai perangkat elektronik tepatnya diperangkat Komputer, Laptop, dan Smartphone
+            Dan lain-lain.
+        </p>
+        <hr>
+        <div class="row ml-1">
+            <div class="w-100">
+                <h3><i class="fa fa-map-marker"></i>Alamat</h3>
+                <p>Pamulang<br>Tangerang Selatan<br>Banten<br>Indonesia<br><strong>M. Faisal</strong></p>
+            </div>
+            <!-- /.col-sm-4-->
+            <div class="">
+                <h3><i class="fa fa-phone"></i> Hubungi Kami</h3>
+                <p class="text-muted">Anda dapat menghubungi kami dinomer yang tertera dibawah ini</p>
+                <p><strong>+628 57 1047 5251</strong></p>
+            </div>
+        </div>
+    </div>
+</div>
+<?php require 'templates/footer.php' ?>
